c4a4a51382748f7c7a3e5744c2794b2be3c9894d
Sahil Rajput <sahilrajput03@gmail.com>
2020-05-29 14:16:16 +0530

    Promise.js-Added

--- promise.js
+++ promise.js
@@ -2,6 +2,21 @@ let what = true;
 let pa = new Promise((resolve, reject) =>
   setTimeout(() => (what ? resolve(687) : reject(786)), 3_000)
 );
-pa.then((j) => console.log("The value of j is ", j)).catch((r) =>
-  console.log("The value of r is ", r)
-);
+// pa.then((j) => console.log("The value of j is ", j)).catch((r) =>
+//   console.log("The value of r is ", r)
+// );
+let retunedValue = 29939;
+
+function kaka() {
+  return pa
+    .then((j) => {
+      console.log("Scope of then - ", retunedValue);
+      retunedValue = j;
+      console.log("Scope of then2 - ", retunedValue);
+      return j;
+    })
+    .catch((r) => r);
+}
+// kaka().then((k) => console.log(k));
+kaka().then(() => console.log("Scope of then3 - ", retunedValue, "Amazing..."));
+console.log("First class - ", retunedValue);
